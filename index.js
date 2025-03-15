8e64a6c71ae17b880be0c4d961679a9d7333e648
unknown <prantokih42@gmail.com>
2025-03-15 23:49:07 +0600

    jwt admin added

--- index.js
+++ index.js
@@ -30,11 +30,33 @@ async function run() {
     // Send a ping to confirm a successful connection
     const  usersCollection = client.db("BrightBites").collection("users");
     const  doctorCollection = client.db("BrightBites").collection("doctors");
+    //jwt related api
+    app.post('/jwt',async (req, res) => {
+      const user = req.body;
+      const token =  jwt.sign(user, process.env.ACCESS_TOKEN_SECRET,{expiresIn: '1d'})
+      res.send({token})
+    })
 
-    app.get('/users', async(req, res) => {
+    const verifyToken = (req, res, next) => {
+     if(req.headers.authorization){
+      return res.status(401).send({message: 'unauthorized access'})
+     }
+     const token = req.headers.authorization.split(' ')[1];
+     
+     jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+        if(err){
+          return res.status(401).send({message: 'unauthorized access'})
+        }
+        req.decoded = decoded;
+        next();
+     })
+    }
+
+    app.get('/users',verifyToken, async(req, res) => {
         const result = await usersCollection.find().toArray();
         res.send(result)
-    })
+    });
+
 
     app.get('/users/:email', async (req, res) => {
         const email = req.params.email
@@ -52,6 +74,35 @@ async function run() {
         const result = await usersCollection.insertOne(user);
         res.send(result);
       })
+      
+     app.get('/users/admin/:email', async (req, res) => {
+      const email = req.params.email;
+      if(email !== req.decoded.email){
+        return res.status(403).send({message: 'unauthorized access'})
+      }
+      const query = { email: email }  ;
+      const user =  await usersCollection.findOne(query);
+      let admin = false;
+      if(user?.role === 'admin'){
+        admin = user?.role === 'admin'
+      }
+      res.send({admin})
+     })
+      
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
