bc0bf4602cc3b49ee555d76c9686c16c7ee2dfb4
unknown <prantokih42@gmail.com>
2025-03-17 23:48:12 +0600

    doctor post added

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const express  = require('express')
+const express = require('express')
 const app = express()
 const cors = require('cors');
 const jwt = require('jsonwebtoken');
@@ -28,78 +28,87 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     // await client.connect();
     // Send a ping to confirm a successful connection
-    const  usersCollection = client.db("BrightBites").collection("users");
-    const  doctorCollection = client.db("BrightBites").collection("doctors");
+    const usersCollection = client.db("BrightBites").collection("users");
+    const doctorCollection = client.db("BrightBites").collection("doctors");
     //jwt related api
-    app.post('/jwt',async (req, res) => {
+    app.post('/jwt', async (req, res) => {
       const user = req.body;
-      const token =  jwt.sign(user, process.env.ACCESS_TOKEN_SECRET,{expiresIn: '1d'})
-      res.send({token})
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1d' })
+      res.send({ token })
     })
 
     const verifyToken = (req, res, next) => {
-     if(req.headers.authorization){
-      return res.status(401).send({message: 'unauthorized access'})
-     }
-     const token = req.headers.authorization.split(' ')[1];
-     
-     jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
-        if(err){
-          return res.status(401).send({message: 'unauthorized access'})
+      if (!req.headers.authorization) {
+        return res.status(401).send({ message: 'unauthorized access' })
+      }
+      const token = req.headers.authorization.split(' ')[1];
+
+      jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+        if (err) {
+          return res.status(401).send({ message: 'unauthorized access' })
         }
         req.decoded = decoded;
         next();
-     })
+      })
     }
     const verifyAdmin = async (req, res, next) => {
       const email = req.decoded.email;
-      const query = {email: email}
+      const query = { email: email }
       const user = await usersCollection.findOne(query);
       const isAdmin = user?.role === 'admin';
-      if(!isAdmin){
-        return res.status(403).send({message: 'unauthorized access'})
+      if (!isAdmin) {
+        return res.status(403).send({ message: 'unauthorized access' })
       }
       next();
     }
 
-    app.get('/users',verifyToken, async(req, res) => {
-        const result = await usersCollection.find().toArray();
-        res.send(result)
+    app.get('/users', verifyToken, verifyAdmin, async (req, res) => {
+      const result = await usersCollection.find().toArray();
+      res.send(result)
     });
 
 
     app.get('/users/:email', async (req, res) => {
-        const email = req.params.email
-        const query = { email: email }
-        const result = await usersCollection.findOne(query)
-        res.send(result)
-      })
-      app.post('/users', async (req, res) => {
-        const user = req.body;
-        const query = { email: user.email }
-        const existsUser = await usersCollection.findOne(query);
-        if (existsUser) {
-          return res.send({ message: 'user already exist', insertedId: null })
-        }
-        const result = await usersCollection.insertOne(user);
-        res.send(result);
-      })
-      
-     app.get('/users/admin/:email',verifyToken, async (req, res) => {
-      const email = req.params.email;
-      if(email !== req.decoded.email){
-        return res.status(403).send({message: 'unauthorized access'})
+      const email = req.params.email
+      const query = { email: email }
+      const result = await usersCollection.findOne(query)
+      res.send(result)
+    })
+    app.post('/users', async (req, res) => {
+      const user = req.body;
+      const query = { email: user.email }
+      const existsUser = await usersCollection.findOne(query);
+      if (existsUser) {
+        return res.send({ message: 'user already exist', insertedId: null })
       }
-      const query = { email: email }  ;
-      const user =  await usersCollection.findOne(query);
+      const result = await usersCollection.insertOne(user);
+      res.send(result);
+    })
+
+    app.get('/users/admin/:email', verifyToken, async (req, res) => {
+      const email = req.params.email;
+      if (email !== req.decoded.email) {
+        return res.status(403).send({ message: 'unauthorized access' })
+      }
+      const query = { email: email };
+      const user = await usersCollection.findOne(query);
       let admin = false;
-      if(user){
+      if (user) {
         admin = user?.role === 'admin'
       }
       res.send({ admin })
-     })
-      
-
+    })
+    // doctor related
+    app.get('/doctors', async (req, res) => {
+      const result = await doctorCollection.find().toArray();
+      res.send(result)
+    })
+   
+    app.post('/doctors',verifyToken,verifyAdmin, async (req, res) => {
+      const doctor = req.body;
+      const result = await doctorCollection.insertOne(doctor);
+      res.send(result);
+    })
 
 
 
@@ -117,7 +126,7 @@ async function run() {
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
-   
+
     // await client.close();
   }
 }
@@ -125,9 +134,9 @@ run().catch(console.dir);
 
 
 app.get('/', (req, res) => {
-    res.send('Doctor is checking')
+  res.send('Doctor is checking')
 })
 
 app.listen(port, () => {
- console.log(`Doctor is Checking on ${port}`)
+  console.log(`Doctor is Checking on ${port}`)
 })
